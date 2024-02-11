db933583ea785fc7edbd71bc6234cc5ce47f577a
edsonfreitas <edsonfreitasarruda872@gmail.com>
2024-02-10 21:11:14 -0300

    feat: Making the application responsive

--- src/app.tsx
+++ src/app.tsx
@@ -43,7 +43,7 @@ export function App() {
 
 
   return (
-    <main className="mx-auto max-w-6xl my-12 space-y-6">
+    <main className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo}  alt="Logo Edson"/>
 
       <form className="w-full">
@@ -58,7 +58,7 @@ export function App() {
       <div className="h-px bg-details" />
 
       <div
-        className="grid grid-cols-3 gap-6 auto-rows-[250px]"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]"
         >
         <NewNote onNoteCreated={onNoteCreated} />
 
